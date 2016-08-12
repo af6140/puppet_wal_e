65621c44787fe43bd74fc2087b5995496c7533fe
Wang, Dawei <dwang@entertainment.com>
2016-08-12 17:19:07 -0400

    validation.

--- manifests/googleconfig.pp
+++ manifests/googleconfig.pp
@@ -16,6 +16,8 @@ class wal_e::googleconfig(
   }
 
   if $google_applcation_credentials {
+    #it is a json file
+    validate_absolute_path($google_applcation_credentials)
     file { "${::wal_e::env_dir}/env/GOOGLE_APPLCATION_CREDENTIALS":
       ensure => present,
       owner => $wal_e::user,
